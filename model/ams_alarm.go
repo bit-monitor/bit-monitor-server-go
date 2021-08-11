cc3793d911ba5cef7b9ab8f35d9b7b0ae7bf82bc
shaolonger <shaolong.chuang@qq.com>
2021-08-11 23:25:13 +0800

    fix: /alarm/add接口，修复rule字段文字长度限制的问题

--- model/ams_alarm.go
+++ model/ams_alarm.go
@@ -7,7 +7,7 @@ type AmsAlarm struct {
 	Name              string    `json:"name" gorm:"not null;type:varchar(100);comment:预警名称"`
 	Level             int8      `json:"level" gorm:"not null;type:tinyint(1);;comment:报警等级，-1-P4低，0-P3中，1-P2高，2-P1紧急"`
 	Category          int8      `json:"category" gorm:"not null;type:tinyint(1);comment:过滤条件。0-全部，1-JS_ERROR，2-HTTP_ERROR，3-RESOURCE_LOAD，4-CUSTOM_ERROR"`
-	Rule              string    `json:"rule" gorm:"not null;type:varchar(255);comment:预警规则，存放JSON格式"`
+	Rule              string    `json:"rule" gorm:"not null;type:varchar(500);comment:预警规则，存放JSON格式"`
 	StartTime         string    `json:"startTime" gorm:"not null;type:varchar(8);comment:报警时段-开始时间，例如00:00:00"`
 	EndTime           string    `json:"endTime" gorm:"not null;type:varchar(8);;comment:报警时段-结束时间，例如23:59:59"`
 	SilentPeriod      int8      `json:"silentPeriod" gorm:"not null;type:tinyint(1);comment:静默期，0-不静默，1-5分钟，2-10分钟，3-15分钟，4-30分钟，5-1小时，6-3小时，7-12小时，8-24小时，9-当天"`
